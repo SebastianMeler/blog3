6fb76928a3cf799de095329b48c00e1fd19ec243
Sebastian <sebastianissimus@gmail.com>
2021-09-10 12:49:34 +0200

    added docker, requirements update

--- mysite/settings.py
+++ mysite/settings.py
@@ -10,6 +10,9 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+if DEBUG:
+    ALLOWED_HOSTS = ['localhost']
+
 
 INSTALLED_APPS = [
     'django.contrib.admin',
@@ -55,8 +58,12 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': 'db',
+        'PORT': 5432,
     }
 }
 
